9a071859138651154a194adde45b6bbc978089e1
Sarthak Sharma <sarthaksharma@Sarthaks-Laptop.local>
2024-03-13 21:51:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+function locomotiveAnimation() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true,
+
+        // for tablet smooth
+        tablet: { smooth: true },
+
+        // for mobile
+        smartphone: { smooth: true }
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length
+                ? locoScroll.scrollTo(value, 0, 0)
+                : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+        }
+
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+
+}
+
+locomotiveAnimation()
+
+gsap.from("#page1 h1",{
+    // opacity:0,
+    y:600,
+    duration:1,
+    delay:0.5,
+    stagger:0.2,
+})
