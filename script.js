5cb4ffd2527ce8123a12bcce611aa1eb77b6d44e
Sarthak Sharma <sarthaksharma@Sarthaks-Laptop.local>
2024-03-16 20:42:09 +0530

    made page3 animation

--- script.js
+++ script.js
@@ -36,24 +36,70 @@ function locomotiveAnimation() {
 
 }
 
-// locomotiveAnimation()
+locomotiveAnimation()
 
-var allText = document.querySelectorAll(".page1-text h1")
+function cursorAnimation() {
 
-allText.forEach(function(elem){
-    var text = elem.textContent
-    var splittedText = text.split("")
-    var clutter = ""
-    splittedText.forEach(function(e){
-        clutter += `<span>${e}</span>`
+    var crsr = document.querySelector("#cursor")
+
+    document.addEventListener("mousemove", function (dets) {
+        gsap.to(crsr, {
+            x: dets.x,
+            y: dets.y
+        })
     })
-    elem.innerHTML = clutter
-})
 
-gsap.from(".page1-text h1 span",{
-    y:300,
-    opacity:0,
-    delay:0.5,
-    duration:0.6,
-    stagger:0.1
-})
+    var allImages = document.querySelectorAll(".image-div")
+    var text = ""
+    allImages.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
+            text = elem.getAttribute("data-text")
+            // crsr.style.width = "160px"
+            gsap.to(crsr, {
+                width: "170px"
+            })
+            gsap.from("#cursor h5", {
+                opacity: 0,
+                delay: 0.2
+            })
+            crsr.innerHTML = `<h5>${text}</h5> <h5>${text}</h5> <h5>${text}</h5>`
+        })
+        elem.addEventListener("mouseleave", function () {
+            // crsr.style.width = "25px"
+            crsr.innerHTML = "<h5></h5>"
+            gsap.to(crsr, {
+                width: "25px"
+            })
+        })
+    })
+}
+
+cursorAnimation()
+
+function page1Text() {
+    var allText = document.querySelectorAll(".page1-text h1")
+
+    allText.forEach(function (elem) {
+        var text = elem.textContent
+        var splittedText = text.split("")
+        var clutter = ""
+        splittedText.forEach(function (e) {
+            clutter += `<span>${e}</span>`
+        })
+        elem.innerHTML = clutter
+    })
+
+
+
+    gsap.from(".page1-text h1 span", {
+        y: 300,
+        opacity: 0,
+        delay: 0.5,
+        duration: 0.6,
+        stagger: 0.1
+    })
+
+
+}
+
+page1Text()
